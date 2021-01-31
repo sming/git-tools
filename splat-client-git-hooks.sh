63f4f866062c56af55321dc7ded7bdb3e537de48
Peter Kingswell <peterk@spotify.com>
2021-01-31 11:13:33 -0500

    tidied up output from script

--- splat-client-git-hooks.sh
+++ splat-client-git-hooks.sh
@@ -6,24 +6,31 @@
 #----------------------------------------------------------------
 # Usage: pass in the TLD of all your checked-out repos, namely ~/src
 #----------------------------------------------------------------
+SCRIPT_DIR=`dirname $0`
+echo "This script's directory: $SCRIPT_DIR"
 
+
+CURRENT_DIR=`pwd`
+echo "Directory invoked from: $CURRENT_DIR"
 
 TARGET_DIR=$1
-echo "TARGET_DIR: $TARGET_DIR"
+echo "Target directory to search for repos: $TARGET_DIR"
 
 REPOS=`find "$TARGET_DIR" -name '.git' -type d -maxdepth 3`
+
+echo "———————"
+echo "Starting to splat..."
+echo "———————"
 
 for REPO in $REPOS
 do
     echo "Splatting hooks to $REPO ..."
 
-    SCRIPT_DIR=`dirname $0`
-    echo "SCRIPT_DIR: $SCRIPT_DIR"
+    cp -fR "${SCRIPT_DIR}/dot-git/hooks/" ${REPO}/hooks/
 
-    CURRENT_DIR=`pwd`
-    echo "CURRENT_DIR: $CURRENT_DIR"
-
-    cp -fvR "${SCRIPT_DIR}/dot-git/hooks/" ${REPO}/hooks/
-
-    chmod -vR +x "${REPO}/hooks/"
+    chmod -R +x "${REPO}/hooks/"
 done
+
+echo "———————"
+echo "... done splatting."
+echo "———————"
